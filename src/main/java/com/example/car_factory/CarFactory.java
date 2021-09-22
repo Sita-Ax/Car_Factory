a34c3a913833ad6926aacbe3aeb8bbe4193cec21
Sita-Ax <sita81@hotmail.com>
2021-09-22 11:01:14 +0200

    test_color is successful by Filip, Robin, Patrik, Rosita

--- src/main/java/com/example/car_factory/CarFactory.java
+++ src/main/java/com/example/car_factory/CarFactory.java
@@ -1,4 +1,22 @@
 package com.example.car_factory;
 
 public class CarFactory {
+
+    String color;
+
+    public CarFactory(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+
+
 }
--- src/test/java/com/example/car_factory/CarFactoryApplicationTests.java
+++ src/test/java/com/example/car_factory/CarFactoryApplicationTests.java
@@ -4,21 +4,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CarFactoryApplicationTests {
 
     CarFactory carFactory;
+
     @Test
     void contextLoads() {
     }
 
     @BeforeEach
-    void setUp() {
-        carFactory = new CarFactory();
+    void setUp( ) {
+        carFactory = new CarFactory("blue");
     }
 
     @Test
     void test_colors() {
-
+        assertEquals("blue", carFactory.getColor());
     }
 }
