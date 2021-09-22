f285cfe2109cfb431c2d1b7477e6c5e4634493e0
Sita-Ax <sita81@hotmail.com>
2021-09-22 10:46:00 +0200

    CarFactory by Filip, Robin, Patrik, Rosita

---
+++ src/main/java/com/example/car_factory/CarFactory.java
@@ -0,0 +1,4 @@
+package com.example.car_factory;
+
+public class CarFactory {
+}
--- src/test/java/com/example/car_factory/CarFactoryApplicationTests.java
+++ src/test/java/com/example/car_factory/CarFactoryApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.car_factory;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarFactoryApplicationTests {
 
+    CarFactory carFactory;
     @Test
     void contextLoads() {
     }
 
+    @BeforeEach
+    void setUp() {
+        carFactory = new CarFactory();
+    }
+
+    @Test
+    void test_colors() {
+
+    }
 }
